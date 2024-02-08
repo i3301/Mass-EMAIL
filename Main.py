64ff44ea6e33add0f149a477194fe60a6e894c2a
i3301 <158025249+i3301@users.noreply.github.com>
2024-02-07 21:55:07 -0800

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,55 @@
+#Put Google App Password in AppPass.txt
+#Then put list of target emails in Emails.txt
+#Now run Start.bat
+import smtplib
+from colorama import Fore
+
+def print_banner():
+    try:
+        with open("banner.txt", 'r', encoding='utf-8') as file:
+            banner_content = file.read()
+            print(f"{banner_content}")
+    except FileNotFoundError:
+        print(f"Error: File 'banner.txt' not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    print_banner()
+    
+#Variables
+List = "Emails.txt"
+AppPassFile = "AppPass.txt"
+
+#Inputs
+Email = input(f"{Fore.CYAN}[*]Sender Email: ")
+subject = input(f"{Fore.CYAN}[*]SUBJECT: ")
+message = input(f"{Fore.CYAN}[*]MESSAGE: ")
+
+text = f"Subject: {subject}\n\n{message}"
+
+#Target List
+with open(List) as file:
+    Target_List = [line.strip() for line in file.readlines()]
+
+#AppPass
+with open(AppPassFile) as file:
+    AppPass = file.readline().strip()
+
+#Send Function
+def sendEmail():
+    print(f"{Fore.CYAN}[+]Sending Emails. . .")
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+
+    server.login(Email, AppPass)
+
+    server.sendmail(Email, Target_List, text)
+
+    print(f"{Fore.GREEN}[*]Emails Sent With Message",{message})
+    input(f"{Fore.CYAN}[!]Press Enter To Exit. . .")
+
+#Calling The Function
+if __name__ == "__main__":
+    sendEmail()
